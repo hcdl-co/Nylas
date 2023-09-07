7ec440ed5cbbefe922ec409ba71f56a30ab98ddc
bmaggiano <brandon.maggiano@gmail.com>
2023-09-07 09:57:13 -0700

    text edit

--- src/app/components/form.js
+++ src/app/components/form.js
@@ -172,7 +172,7 @@ export default function Form() {
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{" "}
+            Not a member already?{" "}
             <a
               href="#"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
