c1fa051e6422d5d90d917835debcc19ae932ceb1
bmaggiano <brandon.maggiano@gmail.com>
2023-09-07 12:37:48 -0700

    updated config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async generateStaticParams() {
-        // Return an array of paths you want to statically generate
-        return ['/']; // Add more paths as needed
-      },
       output: 'export'
 }
 
